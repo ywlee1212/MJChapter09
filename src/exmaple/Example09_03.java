73e1508d8bb7f6fc473ed89c6ae7d505d42f933a
skm0815 <skm0815@gmail.com>
2020-08-08 16:20:15 +0900

    twosome place 20200808

---
+++ src/exmaple/Example09_03.java
@@ -0,0 +1,12 @@
+package exmaple;
+
+import javax.swing.JFrame;
+
+public class Example09_03 extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
