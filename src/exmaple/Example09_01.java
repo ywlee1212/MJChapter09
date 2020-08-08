88efdbf997aaa083e1d1a7565add32318f7005d9
ywlee1212 <ywlee1212@gmail.com>
2020-08-08 11:04:45 +0900

    학교에서 수정 sub Btn

--- src/exmaple/Example09_01.java
+++ src/exmaple/Example09_01.java
@@ -7,22 +7,24 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Example09_01 extends JFrame {
-	
+
 	Container ct;
 	JButton addBtn, subBtn, mulBtn, divBtn, calBtn;
-	
+
 	public Example09_01() {
 
-		ct=this.getContentPane();
+		ct = this.getContentPane();
 		ct.setLayout(new FlowLayout());
-		
+
 		this.setTitle("예제 09-01");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		addBtn=new JButton("ADD");
+
+		addBtn = new JButton("ADD");
 		ct.add(addBtn);
-		
-		
+
+		subBtn = new JButton("SUB");
+		ct.add(subBtn);
+
 		this.setSize(300, 300);
 		this.setVisible(true);
 	}
