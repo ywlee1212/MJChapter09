8217e05e55c484fa51dc9840037c895f94a5bed7
ywlee1212 <ywlee1212@gmail.com>
2020-08-08 20:07:34 +0900

    거의 완성

--- src/exmaple/Example09_03.java
+++ src/exmaple/Example09_03.java
@@ -1,12 +1,25 @@
 package exmaple;
 
+import java.awt.Container;
+
 import javax.swing.JFrame;
 
 public class Example09_03 extends JFrame {
+	
+	Container ct;
+	
+	public Example09_03() {
+		// TODO Auto-generated constructor stub
+		this.setTitle("예제09-03");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setSize(400, 400);
+		this.setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Example09_03();
 	}
 
 }
