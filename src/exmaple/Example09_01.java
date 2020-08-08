1cf0140933df13b466318ea647b9c1e21699c2cc
ywlee1212 <ywlee1212@gmail.com>
2020-08-08 10:11:57 +0900

    예제09-01 첫번째 커밋
    모두 새로 시작함

---
+++ src/exmaple/Example09_01.java
@@ -0,0 +1,21 @@
+package exmaple;
+
+import javax.swing.JFrame;
+
+public class Example09_01 extends JFrame {
+	
+	public Example09_01() {
+		// TODO Auto-generated constructor stub
+		this.setTitle("예제 09-01");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setSize(300, 300);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Example09_01();
+	}
+
+}
