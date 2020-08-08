47c63a47df1531514862aa7d483f05ee036884c9
ywlee1212 <ywlee1212@gmail.com>
2020-08-08 10:51:47 +0900

    집에서 수정한 후 커밋
    집-학교-GitHub 연동 실험

--- src/exmaple/Example09_01.java
+++ src/exmaple/Example09_01.java
@@ -1,19 +1,27 @@
 package exmaple;
 
 import java.awt.Container;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Example09_01 extends JFrame {
 	
 	Container ct;
+	JButton addBtn, subBtn, mulBtn, divBtn, calBtn;
 	
 	public Example09_01() {
 
 		ct=this.getContentPane();
+		ct.setLayout(new FlowLayout());
 		
 		this.setTitle("예제 09-01");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		addBtn=new JButton("ADD");
+		ct.add(addBtn);
+		
 		
 		this.setSize(300, 300);
 		this.setVisible(true);
